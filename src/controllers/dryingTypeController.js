e5f7c5d3fb2cdd3cfdcbfe8768b10261e72b920e
Lahiru Wijesinghe <wml.eranga@gmail.com>
2025-09-21 07:36:12 +0530

    deployment commit

--- src/controllers/dryingTypeController.js
+++ src/controllers/dryingTypeController.js
@@ -1,5 +1,7 @@
 const DryingType = require("../models/DryingType");
 
+// Controller for managing drying types
+
 class DryingTypeController {
   // GET /api/drying-types - Get all drying types with pagination
   static async getDryingTypes(req, res) {
